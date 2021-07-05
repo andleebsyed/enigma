e6ccf4e37f3a6c8abe6f1d2640837673916c0097
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-05 17:53:12 +0530

    update : initial setup
    configured the project with tailwindCss , made intelliSense for tailwind classes work and removed the unnecessary things from the project.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+
+
+function App() {
+  return (
+    <div className="">
+   <h1 className="font-bold">setup of app done</h1>
+    </div>
+  );
+}
+
+export default App;
