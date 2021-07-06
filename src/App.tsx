3f9387edba81c57a4aa247b3847ffa3667f0b26a
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-06 13:13:33 +0530

    update : moved Header to App.jsx and kept it global

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Homepage } from './pages/Homepage/Homepage'
 import { Categories } from './pages/Categories/Categories';
+import { Header } from './Components/Header/Header';
 import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     
       <div className="bg-grey h-screen">
+        <Header />
         <Routes>
         <Route path="/" element={<Homepage />} />
       <Route path="/categories" element={<Categories />} />
--- src/pages/Homepage/homepage.tsx
+++ src/pages/Homepage/homepage.tsx
@@ -1,11 +1,9 @@
-import { Header } from "../../Components/Header/Header";
 import { Intro } from "../../Components/Intro/Intro";
 import { SignIn } from "../../Components/SignIn/SignIn";
 
 export function Homepage() {
     return (
         <div>
-            <Header />
             <main className="flex flex-row justify-around flex-wrap bg-grey">
                 <Intro />
                 <SignIn />
