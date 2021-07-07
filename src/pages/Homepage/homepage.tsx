913089414ad4022c4a0cb34ad5446f28f90bb229
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-07 16:02:34 +0530

    refactor: prettier doing its job

--- src/pages/Homepage/homepage.tsx
+++ src/pages/Homepage/homepage.tsx
@@ -2,12 +2,12 @@ import { Intro } from "../../Components/Intro/Intro";
 import { SignIn } from "../../Components/SignIn/SignIn";
 
 export function Homepage() {
-    return (
-        <div>
-            <main className="flex flex-row justify-around flex-wrap bg-grey">
-                <Intro />
-                <SignIn />
-            </main>
-        </div>
-    )
+  return (
+    <div>
+      <main className="flex flex-row justify-around flex-wrap bg-grey">
+        <Intro />
+        <SignIn />
+      </main>
+    </div>
+  );
 }
