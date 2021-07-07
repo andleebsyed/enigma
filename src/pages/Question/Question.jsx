e3ab190d699fbf28fbef0723514866386aa02d43
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-07 14:47:32 +0530

    update : embedded different options to Question component

--- src/pages/Question/Question.jsx
+++ src/pages/Question/Question.jsx
@@ -1,7 +1,21 @@
+import { General } from "../../Components/Options/General"
+import { Right } from "../../Components/Options/Right"
+import { Wrong } from "../../Components/Options/Wrong"
+
 export function Question() {
+    
     return (
-        <div>
-            <h1>i am in  question page</h1>
+        <div className="min-h-screen bg-grey">
+            <h1 className="text-white font-bold m-2">Question 0/0</h1>
+            <main className="flex flex-col items-center ml-2 mr-2 mt-16 ">
+                <div className="bg-grey-extralight rounded mb-4 md:w-4/5">
+                    <p className="font-extrabold text-white break-all p-4">Question goes uygfweui y jhdwyh jhdgfh jsdchv jkhsfre yhfuj  sidusiud iusdisuf ikuiku</p>
+                </div>
+                <General />
+                <Right />
+                <Wrong />
+                <General />
+            </main>
         </div>
     )
 }
