b87d9052108200c539561b6a7d8a2c8a462ac8ff
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-07 16:02:15 +0530

    refactor : extracted question div  of question page to a separate component

---
+++ src/Components/QuizQuestion/QuizQuestion.tsx
@@ -0,0 +1,10 @@
+export function QuizQuestion() {
+  return (
+    <div className="bg-grey-extralight rounded mb-4 lg:w-3/5 md:w-4/5 sm:h-20">
+      <p className="font-extrabold text-white break-all p-4 ">
+        Question goes uygfweui y jhdwyh jhdgfh jsdchv jkhsfre yhfuj sidusiud
+        iusdisuf ikuiku
+      </p>
+    </div>
+  );
+}
--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,22 +1,21 @@
 import { General } from "../../Components/Options/General"
 import { Right } from "../../Components/Options/Right"
 import { Wrong } from "../../Components/Options/Wrong"
-import { QuestionPageQuizInfo } from "../../Components/QuestionPageQuizInfo/QuestionPageQuizInfo"
+import { QuizPerformance } from "../../Components/QuizPerformance/QuizPerformance"
+import { QuizQuestion } from "../../Components/QuizQuestion/QuizQuestion"
 
 export function Question() {
     
     return (
         <div className="min-h-screen bg-grey">
             {/* <section className="flex justify-between">
-                <span className="text-white font-bold m-2">Question 0/0</span>
-                <span className="text-white font-bold m-2">Score:100</span>
+                <span className="text-white font-bold m-2">Question 0/0</span> kjkjkj
+                <span className="text-white font-bold m-2">Score:100</span>  
             </section> */}
-            <QuestionPageQuizInfo />
+            <QuizPerformance /> 
             
             <main className="flex flex-col items-center ml-2 mr-2 mt-16 ">
-                <div className="bg-grey-extralight rounded mb-4 md:w-4/5">
-                    <p className="font-extrabold text-white break-all p-4">Question goes uygfweui y jhdwyh jhdgfh jsdchv jkhsfre yhfuj  sidusiud iusdisuf ikuiku</p>
-                </div>
+                <QuizQuestion />
                 {/* options */}
                 <General />
                 <Right />
