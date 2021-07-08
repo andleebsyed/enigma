dd2a1f2d824ffabcd4ceaccfcfb9702b8e8b2e5f
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-08 14:49:14 +0530

    style : changed positioning of cards in Categories component

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,19 +1,16 @@
 import { QuizCard } from "../../Components/QuizCard/QuizCard";
 
 export function Categories() {
-    return (
-        <main className="flex justify-evenly flex-wrap bg-grey mt-4">
-            <QuizCard />
-            <QuizCard />
-            <QuizCard />
-            <QuizCard />
-       </main>
-
-
-        
-
-      
-
-      
-    )
+  return (
+    <div className="flex flex-col justify-center bg-grey items-center min-h-screen ">
+      <section className="flex justify-evenly flex-wrap  ">
+        <QuizCard />
+        <QuizCard />
+      </section>
+      <section className="flex justify-evenly flex-wrap ">
+        <QuizCard />
+        <QuizCard />
+      </section>
+    </div>
+  );
 }
