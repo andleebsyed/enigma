ae8cbf0ce5a18a1887eb74ec648afa7ed6d5b99e
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:34:35 +0530

    update: populated Categories component with dynamic data

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,15 +1,14 @@
 import { QuizCard } from "../../Components/QuizCard/QuizCard";
-
+import { useData } from "../../context/quizdata-context";
+// import { data } from "../../data/data";
 export function Categories() {
+  const data = useData();
   return (
     <div className="flex flex-col justify-center bg-grey items-center min-h-screen ">
       <section className="flex justify-evenly flex-wrap  ">
-        <QuizCard />
-        <QuizCard />
-      </section>
-      <section className="flex justify-evenly flex-wrap ">
-        <QuizCard />
-        <QuizCard />
+        {data?.quizCategories.map((category) => (
+          <QuizCard key={category.quizName} category={category} />
+        ))}
       </section>
     </div>
   );
