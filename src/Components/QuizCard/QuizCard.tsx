74341a4866f769ee16d01b3308474238e5a449b4
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:32:11 +0530

    update: defined the type for function argument

--- src/Components/QuizCard/QuizCard.tsx
+++ src/Components/QuizCard/QuizCard.tsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+// import { Question } from "../../pages/Question/Question";
 
-export function QuizCard() {
-    return (
-        <section className=" bg-grey-dark h-72 w-56 flex flex-col mt-4 m-4 items-center rounded">
-                <h1 className="text-blue text-2xl font-bold mb-2">Aqua</h1>
-                <img alt="placeholder" src="https://via.placeholder.com/150" />
-            <span className="text-white mx-4 text-center ">Do you know about marine life</span>
-            <Link to="/question">
-            <button className="p-1 m-1  w-40 bg-red text-white font-semibold  rounded">Play Now</button>
-            </Link>
-                
-            </section>
-    )
+export type CATEGORY = {
+  category: {
+    quizName: string;
+    description: string;
+    questions: {
+      question: string;
+      options: string[];
+      correctOption: string;
+    }[];
+  };
+};
+export function QuizCard({ category }: CATEGORY) {
+  return (
+    <section className=" bg-grey-dark h-72 w-56 flex flex-col mt-4 m-4 items-center justify-evenly rounded">
+      <h1 className="text-blue text-2xl font-bold mb-2">{category.quizName}</h1>
+      {/* <img alt="placeholder" src="https://via.placeholder.com/150" /> */}
+      <span className="text-white mx-4 text-center ">
+        {category.description}
+      </span>
+      <Link to={`/question/${category.quizName}`}>
+        <button className="p-1 m-1  w-40 bg-red text-white font-semibold  relative top-40-0 rounded">
+          Play Now
+        </button>
+      </Link>
+    </section>
+  );
 }
