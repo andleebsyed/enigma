1f6de11690c3d866ecc2c0f8fd00558c94e5e091
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:32:41 +0530

    update: defined type

--- src/Components/QuizQuestion/QuizQuestion.tsx
+++ src/Components/QuizQuestion/QuizQuestion.tsx
@@ -1,10 +1,23 @@
-export function QuizQuestion() {
+import { General } from "../Options/General";
+
+type QUESTIONS = {
+  questions: {
+    question: string;
+    options: string[];
+    correctOption: string;
+  }[];
+};
+export function QuizQuestion({ questions }: QUESTIONS) {
   return (
-    <div className="bg-grey-extralight rounded mb-4 lg:w-3/5 md:w-4/5 sm:h-20">
-      <p className="font-extrabold text-white break-all p-4 ">
-        Question goes uygfweui y jhdwyh jhdgfh jsdchv jkhsfre yhfuj sidusiud
-        iusdisuf ikuiku
-      </p>
-    </div>
+    <>
+      <div className="bg-grey-extralight rounded mb-4 lg:w-3/5 md:w-4/5 sm:h-20">
+        <p className="font-extrabold text-white break-all p-4 ">
+          {questions[0].question}
+        </p>
+      </div>
+      {questions[0].options.map((option) => (
+        <General option={option} />
+      ))}
+    </>
   );
 }
