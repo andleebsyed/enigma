079d74f48c4439008afa42ad9d749395e99a9509
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 17:40:23 +0530

    update: made usrer interaction with options of a particular question work. option pops up in green if correct, and in red if wrong

--- src/Components/Options/General.tsx
+++ src/Components/Options/General.tsx
@@ -1,9 +1,23 @@
+import React from "react";
+import { useData } from "../../context/quizdata-context";
+
 type OPTION = {
   option: String;
+  correctOption: String;
 };
-export function General({ option }: OPTION) {
+export function General({ option, correctOption }: OPTION) {
+  // const data = useData()
+  // function optionHitHandler(option: React.MouseEvent) {
+  //   // data?.quizCategories.map()
+  //   if (option === correctOption) {
+  //   }
+  // }
+
   return (
-    <button className="bg-grey-extralight rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6">
+    <button
+      // onClick={(option) => optionHitHandler(option)}
+      className="bg-grey-extralight rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6"
+    >
       <p className="font-extrabold text-white break-all p-4">{option}</p>
     </button>
   );
--- src/Components/QuizQuestion/QuizQuestion.tsx
+++ src/Components/QuizQuestion/QuizQuestion.tsx
@@ -1,7 +1,71 @@
-import { General } from "../Options/General";
+// import { createNoSubstitutionTemplateLiteral } from "typescript";
+// import { Question } from "../../pages/Question/Question";
+// import { General } from "../Options/General";
+import { useState } from "react";
 import { QUESTIONS } from "./QuizQuestions.types";
 
 export function QuizQuestion({ questions }: QUESTIONS) {
+  type BGOPTIONS = {
+    optionColor: String;
+  }[];
+  const [bgOptions, setBgOptions] = useState<string[]>([
+    "bg-grey-extralight",
+    "bg-grey-extralight",
+    "bg-grey-extralight",
+    "bg-grey-extralight",
+  ]);
+  function optionHitHandler(
+    choosenOption: string,
+    correctOption: string,
+    options: string[]
+  ) {
+    // data?.quizCategories.map()
+    //  if (option === correctOption) {
+    //    console.log("green")
+    //  }
+    //  else {
+    //    console.log("red")
+    //  }
+    options.forEach((option, index) => {
+      if (option === choosenOption) {
+        if (option === correctOption) {
+          console.log("green:answered correctly ", option);
+          console.log("indexc is ", index);
+          const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
+            index === bgIndex ? (bgOptions[bgIndex] = "bg-green") : bgOption
+          );
+          setBgOptions(newBgOptions);
+          console.log({ bgOptions });
+        } else {
+          console.log("red:answered incorrectly ", option);
+          const indexOfCorrectOption = bgOptions.indexOf(correctOption);
+          const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
+            index === bgIndex ? (bgOptions[bgIndex] = "bg-red") : bgOption
+          );
+          setBgOptions(newBgOptions);
+          console.log({ bgOptions });
+        }
+      }
+      if (option === correctOption) {
+        console.log("green: correct option:not choosen have to show");
+        const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
+          index === bgIndex ? (bgOptions[bgIndex] = "bg-green") : bgOption
+        );
+        setBgOptions(newBgOptions);
+        console.log({ bgOptions });
+      }
+      // else {
+      //   console.log("grey: we did not fddle with these ", option);
+      //   const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
+      //     index === bgIndex
+      //       ? (bgOptions[bgIndex] = "bg-grey-extralight")
+      //       : bgOption
+      //   );
+      //   setBgOptions(newBgOptions);
+      //   console.log({ bgOptions });
+      // }
+    });
+  }
   return (
     <>
       <div className="bg-grey-extralight rounded mb-4 lg:w-3/5 md:w-4/5 sm:h-20">
@@ -9,9 +73,25 @@ export function QuizQuestion({ questions }: QUESTIONS) {
           {questions[0].question}
         </p>
       </div>
-      {questions[0].options.map((option) => (
-        <General option={option} />
+      {questions[0].options.map((option, index) => (
+        <button
+          onClick={() =>
+            optionHitHandler(
+              option,
+              questions[0].correctOption,
+              questions[0].options
+            )
+          }
+          className={`${bgOptions[index]}  rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6`}
+        >
+          <p className="font-extrabold text-white break-all p-4">{option}</p>
+        </button>
       ))}
+      <ul>
+        {bgOptions.map((singleOption) => (
+          <li>{singleOption}</li>
+        ))}
+      </ul>
     </>
   );
 }
