f785019dfdc505daba36d893db19740f4f9ae382
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:34:01 +0530

    update: created the context for the data to be available globally

---
+++ src/context/quizdata-context.tsx
@@ -0,0 +1,12 @@
+import React, { createContext, useContext } from "react";
+import { data } from "../data/data";
+import { QUIZCATEGORIES } from "../data/data";
+export const DataContext = createContext<QUIZCATEGORIES | null>(null);
+
+export function DataProvider({ children }: React.PropsWithChildren<{}>) {
+  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+}
+
+export function useData() {
+  return useContext(DataContext);
+}
