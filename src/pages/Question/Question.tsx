9cde359c706f8d82090afd02b201f521e43b50da
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:35:20 +0530

    update: made Question component work according to data which we have defined

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -1,20 +1,36 @@
-import { General } from "../../Components/Options/General";
-import { Right } from "../../Components/Options/Right";
-import { Wrong } from "../../Components/Options/Wrong";
+import { useParams } from "react-router-dom";
+// import { General } from "../../Components/Options/General";
+// import { Right } from "../../Components/Options/Right";
+// import { Wrong } from "../../Components/Options/Wrong";
+// import { CATEGORY } from "../../Components/QuizCard/QuizCard";
 import { QuizPerformance } from "../../Components/QuizPerformance/QuizPerformance";
 import { QuizQuestion } from "../../Components/QuizQuestion/QuizQuestion";
+import { useData } from "../../context/quizdata-context";
+// import { data } from "../../data/data";
 
 export function Question() {
+  const { chosenQuizName } = useParams();
+  const data = useData();
+  console.log("data is ", data);
+  console.log("quiz name is ", chosenQuizName);
+  const chosenQuiz = data?.quizCategories.find((singleQuiz) => {
+    console.log("data values ", singleQuiz.quizName);
+    return String(singleQuiz.quizName) === String(chosenQuizName);
+  });
+  console.log("aahaa here  our quix is ", chosenQuiz);
   return (
     <div className="min-h-screen bg-grey">
       <QuizPerformance />
       <main className="flex flex-col items-center ml-2 mr-2 mt-16 ">
-        <QuizQuestion />
+        {/* {chosenQuiz?.questions.map(question =>  */}
+        <QuizQuestion questions={chosenQuiz!.questions} />
+        {/* )} */}
+        {/* <QuizQuestion questions={chosenQuiz!.questions} /> */}
         {/* options */}
-        <General />
-        <Right />
-        <Wrong />
-        <General />
+        {/* <General /> */}
+        {/* <Right /> */}
+        {/* <Wrong /> */}
+        {/* <General /> */}
         <button className="text-white bg-blue font-bold p-2 w-40  rounded ">
           Skip
         </button>
