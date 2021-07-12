8f9845d7fd8dd3e8a85bfd875b411f5eece62aee
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-12 15:31:34 +0530

    update: transition of General component to showing dynamic data on view

--- src/Components/Options/General.tsx
+++ src/Components/Options/General.tsx
@@ -1,7 +1,10 @@
-export function General() {
-    return (
-        <button className="bg-grey-extralight rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6">
-        <p className="font-extrabold text-white break-all p-4">General Option</p>
+type OPTION = {
+  option: String;
+};
+export function General({ option }: OPTION) {
+  return (
+    <button className="bg-grey-extralight rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6">
+      <p className="font-extrabold text-white break-all p-4">{option}</p>
     </button>
-    )
+  );
 }
