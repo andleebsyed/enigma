3c085061f47f8df743cb50a66ae4d54d90774c98
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-14 00:30:52 +0530

    update: made whole quiz page work, did color change of correct and wrong options etc

--- src/Components/QuizQuestion/QuizQuestion.tsx
+++ src/Components/QuizQuestion/QuizQuestion.tsx
@@ -1,85 +1,86 @@
-// import { createNoSubstitutionTemplateLiteral } from "typescript";
-// import { Question } from "../../pages/Question/Question";
-// import { General } from "../Options/General";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useQuizPerformance } from "../../context/quizPerformance.context";
 import { QUESTIONS } from "./QuizQuestions.types";
 
 export function QuizQuestion({ questions }: QUESTIONS) {
-  type BGOPTIONS = {
-    optionColor: String;
-  }[];
-  const [bgOptions, setBgOptions] = useState<string[]>([
+  const initialState = [
     "bg-grey-extralight",
     "bg-grey-extralight",
     "bg-grey-extralight",
     "bg-grey-extralight",
-  ]);
+  ];
+
+  const [bgOptions, setBgOptions] = useState<string[]>(initialState);
+  const { quizPerformance, setQuizPerformance } = useQuizPerformance();
+  const [questionIteratorIndex, setQuestionIteratorIndex] = useState<number>(0);
+
+  useEffect(() => {
+    console.log("useefffect running");
+    setQuizPerformance({
+      ...quizPerformance,
+      totalQuestions: questions.length,
+    });
+  }, []);
+
+  function ToRunAfterOptionHit(scoreUpdate: number) {
+    // setQuizPerformance({
+    //   ...quizPerformance,
+
+    //   score: quizPerformance!.score + scoreUpdate,
+    // });
+    setTimeout(() => {
+      setQuestionIteratorIndex(questionIteratorIndex + 1);
+      setQuizPerformance({
+        ...quizPerformance,
+        currentQuestion: quizPerformance.currentQuestion + 1,
+        score: quizPerformance.score + scoreUpdate,
+      });
+      setBgOptions(initialState);
+    }, 2000);
+  }
+
+  function skipButtonHandler() {
+    setQuestionIteratorIndex(() => questionIteratorIndex + 1);
+    setQuizPerformance({
+      ...quizPerformance,
+      currentQuestion: quizPerformance.currentQuestion + 1,
+    });
+  }
+
   function optionHitHandler(
     choosenOption: string,
     correctOption: string,
     options: string[]
   ) {
-    // data?.quizCategories.map()
-    //  if (option === correctOption) {
-    //    console.log("green")
-    //  }
-    //  else {
-    //    console.log("red")
-    //  }
-    options.forEach((option, index) => {
-      if (option === choosenOption) {
-        if (option === correctOption) {
-          console.log("green:answered correctly ", option);
-          console.log("indexc is ", index);
-          const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
-            index === bgIndex ? (bgOptions[bgIndex] = "bg-green") : bgOption
-          );
-          setBgOptions(newBgOptions);
-          console.log({ bgOptions });
-        } else {
-          console.log("red:answered incorrectly ", option);
-          const indexOfCorrectOption = bgOptions.indexOf(correctOption);
-          const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
-            index === bgIndex ? (bgOptions[bgIndex] = "bg-red") : bgOption
-          );
-          setBgOptions(newBgOptions);
-          console.log({ bgOptions });
-        }
-      }
-      if (option === correctOption) {
-        console.log("green: correct option:not choosen have to show");
-        const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
-          index === bgIndex ? (bgOptions[bgIndex] = "bg-green") : bgOption
-        );
-        setBgOptions(newBgOptions);
-        console.log({ bgOptions });
-      }
-      // else {
-      //   console.log("grey: we did not fddle with these ", option);
-      //   const newBgOptions = bgOptions.map((bgOption, bgIndex, bgOptions) =>
-      //     index === bgIndex
-      //       ? (bgOptions[bgIndex] = "bg-grey-extralight")
-      //       : bgOption
-      //   );
-      //   setBgOptions(newBgOptions);
-      //   console.log({ bgOptions });
-      // }
-    });
+    const ourOptionIndex = options.indexOf(choosenOption);
+    const newBgOptions = [...bgOptions];
+    if (choosenOption === correctOption) {
+      newBgOptions[ourOptionIndex] = "bg-green";
+      setBgOptions(newBgOptions);
+      ToRunAfterOptionHit(5);
+    } else {
+      const correctOptionIndex = options.indexOf(correctOption);
+      newBgOptions[correctOptionIndex] = "bg-green";
+      newBgOptions[ourOptionIndex] = "bg-red";
+      setBgOptions(newBgOptions);
+      ToRunAfterOptionHit(-2);
+    }
   }
   return (
     <>
       <div className="bg-grey-extralight rounded mb-4 lg:w-3/5 md:w-4/5 sm:h-20">
         <p className="font-extrabold text-white break-all p-4 ">
-          {questions[0].question}
+          {questions[questionIteratorIndex].question}
         </p>
       </div>
-      {questions[0].options.map((option, index) => (
+      {questions[questionIteratorIndex].options.map((option, index) => (
         <button
+          key={index}
           onClick={() =>
             optionHitHandler(
               option,
-              questions[0].correctOption,
-              questions[0].options
+              questions[questionIteratorIndex].correctOption,
+              questions[questionIteratorIndex].options
             )
           }
           className={`${bgOptions[index]}  rounded ml-4 mr-4 mb-4 sm:w-3/6 w-5/6`}
@@ -87,11 +88,12 @@ export function QuizQuestion({ questions }: QUESTIONS) {
           <p className="font-extrabold text-white break-all p-4">{option}</p>
         </button>
       ))}
-      <ul>
-        {bgOptions.map((singleOption) => (
-          <li>{singleOption}</li>
-        ))}
-      </ul>
+      <button
+        onClick={() => skipButtonHandler()}
+        className="text-white bg-blue font-bold p-2 w-40  rounded "
+      >
+        Skip
+      </button>
     </>
   );
 }
