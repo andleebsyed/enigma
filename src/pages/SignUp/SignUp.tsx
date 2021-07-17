4022833c37b84db4dccddc3cd9df658269bff6b2
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-17 18:16:35 +0530

    update: made user signUp work with backend

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,29 +1,114 @@
-import { useNavigate } from "react-router-dom"
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../../ApiUrls/ApiUrls";
 
 export function SignUp() {
-    const navigate = useNavigate()
-    function SignUpSubmitHandler(event :React.SyntheticEvent) {
-        event.preventDefault();
-        navigate('/categories', {replace: true})
-        
+  const initialState = {
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [userData, setUserData] = useState(initialState);
+  const [userSignUpError, setUserSignUpError] = useState({
+    userSignUpErrorMessage: "",
+    userSignUpErrorStatus: "invisible",
+  });
+  const [signUpButtonText, setSignUpButtonText] = useState("Sign Up");
+  const navigate = useNavigate();
+  async function SignUpSubmitHandler(event: React.SyntheticEvent) {
+    event.preventDefault();
+    setSignUpButtonText("Signing Up...");
+    if (userData.password !== userData.confirmPassword) {
+      setUserSignUpError({
+        ...userSignUpError,
+        userSignUpErrorMessage: "Passwords didn't match",
+        userSignUpErrorStatus: "visible",
+      });
+      setSignUpButtonText("Sign Up");
+    } else {
+      const userDetails = {
+        userDetails: {
+          username: userData.username,
+          email: userData.email,
+          password: userData.password,
+        },
+      };
+      const response = await axios.post(BASE_URL + "/user/signup", userDetails);
+      setSignUpButtonText("Sign Up");
+      if (response.data.status) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/categories");
+      } else {
+        setUserSignUpError({
+          ...userSignUpError,
+          userSignUpErrorStatus: "visible",
+          userSignUpErrorMessage: `${response.data.existingField} already exists`,
+        });
+      }
     }
-    return (
-        <div className="flex min-h-screen items-center justify-center bg-grey-dark">
-                  <main className="flex flex-col w-96 p-8  m-3 bg-grey-light rounded-sm">
-        <span className="text-center text-blue font-bold text-xl">Sign Up?</span>
+  }
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-grey-dark">
+      <main className="flex flex-col w-96 p-8  m-3 bg-grey-light rounded-sm">
+        <span className="text-center text-blue font-bold text-xl">
+          Sign Up?
+        </span>
         <form className="flex flex-col" onSubmit={SignUpSubmitHandler}>
-            <label className="text-white mt-8">Username:</label>
-            <input className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold" type="text" name="username" required />
-            <label className="text-white">Email:</label>
-            <input className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold" type="email" name="email" required  />
-            <label className="text-white">Password:</label>
-            <input className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold" type="password" name="password" required />
-            <label className="text-white">Confirm Password:</label>
-            <input className="rounded-sm p-1.5 mb-14 bg-grey-lighter font-bold" type="password" name="confirmPassword" required />   
-            <input className="bg-red p-4 text-white rounded" type="submit" name="signUp" value="Sign Up" />
+          <p
+            className={`text-center text-red font-bold ${userSignUpError.userSignUpErrorStatus}`}
+          >
+            {userSignUpError.userSignUpErrorMessage}
+          </p>
+          <label className="text-white mt-8">Username:</label>
+          <input
+            onChange={(event) =>
+              setUserData({ ...userData, username: event.target.value })
+            }
+            className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold"
+            type="text"
+            name="username"
+            required
+          />
+          <label className="text-white">Email:</label>
+          <input
+            onChange={(event) =>
+              setUserData({ ...userData, email: event.target.value })
+            }
+            className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold"
+            type="email"
+            name="email"
+            required
+          />
+          <label className="text-white">Password:</label>
+          <input
+            onChange={(event) =>
+              setUserData({ ...userData, password: event.target.value })
+            }
+            className="rounded-sm p-1.5 mb-8 bg-grey-lighter font-bold"
+            type="password"
+            name="password"
+            required
+          />
+          <label className="text-white">Confirm Password:</label>
+          <input
+            onChange={(event) =>
+              setUserData({ ...userData, confirmPassword: event.target.value })
+            }
+            className="rounded-sm p-1.5 mb-14 bg-grey-lighter font-bold"
+            type="password"
+            name="confirmPassword"
+            required
+          />
+          <input
+            className="bg-red p-4 text-white rounded"
+            type="submit"
+            name="signUp"
+            value={`${signUpButtonText}`}
+          />
         </form>
-    </main>
-         </div>
-      
-    )
+      </main>
+    </div>
+  );
 }
