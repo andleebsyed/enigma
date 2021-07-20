81d79b93776cc2e6c2a969e2cac06fd1da39ba80
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-20 15:22:01 +0530

    update: added a logout button

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,11 +1,27 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate();
+  const isToken = localStorage.getItem("token");
+
+  function LogoutHandler() {
+    localStorage.clear();
+    navigate("/access", { replace: true });
+  }
+
   return (
-    <header className="flex border-b-4">
+    <header className="flex justify-between border-b-4">
       <Link to="/">
         <h1 className="text-red text-2xl font-bold p-2">Enigma!</h1>
       </Link>
+      {isToken && (
+        <button
+          onClick={LogoutHandler}
+          className={`text-red text-2xl font-bold p-2`}
+        >
+          Logout
+        </button>
+      )}
     </header>
   );
 }
