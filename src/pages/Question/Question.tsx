69b6c5f4b7a21406566d5443717e1bb09d87318c
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-20 15:17:40 +0530

    update: added a loader when data is yet to be fetched

--- src/pages/Question/Question.tsx
+++ src/pages/Question/Question.tsx
@@ -5,16 +5,24 @@ import { useData } from "../../context/quizdata-context";
 
 export function Question() {
   const { chosenQuizName } = useParams();
-  const data = useData();
-  const chosenQuiz = data?.quizCategories.find((singleQuiz) => {
+  const { data } = useData();
+  console.log("data in Question component ", data);
+  console.log("choosen quiz name", chosenQuizName);
+  const chosenQuiz = data.quizCategories.find((singleQuiz) => {
+    console.log("quiz name is ", singleQuiz.quizName);
     return singleQuiz.quizName === chosenQuizName;
   });
-  return (
+  console.log("choiosen quiz is ", chosenQuiz);
+  return data.quizCategories.length > 0 ? (
+    // return (
     <div className="min-h-screen bg-grey">
       <QuizPerformance />
       <main className="flex flex-col items-center ml-2 mr-2 mt-16 ">
         <QuizQuestion questions={chosenQuiz!.questions} />
       </main>
     </div>
+  ) : (
+    // );
+    <div>loading</div>
   );
 }
