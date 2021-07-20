bac3a581584ec5d829cef750098eeac17e9067ff
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-20 15:23:02 +0530

    update: changed data fetching from a js file to database

--- src/context/quizdata-context.tsx
+++ src/context/quizdata-context.tsx
@@ -1,10 +1,17 @@
-import React, { createContext, useContext } from "react";
-import { data } from "../data/data";
-import { QUIZCATEGORIES } from "../data/data.types";
-export const DataContext = createContext<QUIZCATEGORIES | null>(null);
+import React, { createContext, useContext, useState } from "react";
+// import { data } from "../data/data";
+import { QuizDataContext } from "../data/data.types";
+export const DataContext = createContext<QuizDataContext>(
+  {} as QuizDataContext
+);
 
 export function DataProvider({ children }: React.PropsWithChildren<{}>) {
-  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+  const [data, setData] = useState({ quizCategories: [] });
+  return (
+    <DataContext.Provider value={{ data, setData }}>
+      {children}
+    </DataContext.Provider>
+  );
 }
 
 export function useData() {
