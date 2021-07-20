01eb785678d15e65bc20de31ac00ac4be99d7bec
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-20 15:24:20 +0530

    fix: fixed a bug: questions iterated was not being refreshed on playing different quizzes

--- src/pages/Categories/Categories.tsx
+++ src/pages/Categories/Categories.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import { QuizCard } from "../../Components/QuizCard/QuizCard";
 import { useData } from "../../context/quizdata-context";
+import { useQuizPerformance } from "../../context/quizPerformance.context";
 export function Categories() {
-  const data = useData();
+  const { data } = useData();
+  const { quizPerformance, setQuizPerformance } = useQuizPerformance();
+  useEffect(() => {
+    if (quizPerformance.currentQuestion > 1) {
+      setQuizPerformance({ ...quizPerformance, currentQuestion: 1 });
+    }
+  }, []);
+
   return (
     <div className="flex flex-col justify-center bg-grey items-center min-h-screen ">
       <section className="flex justify-evenly flex-wrap  ">
