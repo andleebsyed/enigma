3b9d6d9e09240676c2ab3bb084136fb7d6493730
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-23 11:48:56 +0530

    update: type of token can be string or null

--- src/ApiCalls/userAuth.ts
+++ src/ApiCalls/userAuth.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { BASE_URL } from "../ApiUrls/ApiUrls";
-type SignInResponse = {
+export type SignInResponse = {
   status: boolean;
   allowUser: boolean;
   message: string;
@@ -21,6 +21,7 @@ export type SignUpResponse = {
   status: boolean;
   message: string;
   token: string;
+  username: string;
 };
 export type duplicateError = {
   status: boolean;
@@ -35,7 +36,7 @@ export type ServerError = {
   message: string;
 };
 
-export function setupAuthHeaderForServiceCalls(token: string) {
+export function setupAuthHeaderForServiceCalls(token: string | null) {
   console.log("token applier running");
   if (token) {
     console.log("token found and applied");
@@ -44,24 +45,24 @@ export function setupAuthHeaderForServiceCalls(token: string) {
   console.log("token not found and not applied");
   delete axios.defaults.headers.common["Authorization"];
 }
-export function AuthHeaderHandler(token: string) {
-  axios.interceptors.request.use((req) => {
-    req.headers.authorization = token;
-    console.log("token added for further requests");
-    return req;
-  });
-}
-export function AxiosErrorHandler() {
-  axios.interceptors.response.use(
-    (res) => res,
-    (err) => {
-      if (err.response.status === 404) {
-        throw new Error(`${err.config.url} not found`);
-      }
-      throw err;
-    }
-  );
-}
+// export function AuthHeaderHandler(token: string) {
+//   axios.interceptors.request.use((req) => {
+//     req.headers.authorization = token;
+//     console.log("token added for further requests");
+//     return req;
+//   });
+// }
+// export function AxiosErrorHandler() {
+//   axios.interceptors.response.use(
+//     (res) => res,
+//     (err) => {
+//       if (err.response.status === 404) {
+//         throw new Error(`${err.config.url} not found`);
+//       }
+//       throw err;
+//     }
+//   );
+// }
 export async function UserSignUp(
   userData: UserData
 ): Promise<SignUpResponse | ServerError | duplicateError> {
@@ -155,11 +156,9 @@ export function setupAuthExceptionHandler(navigate: Function) {
     (response) => response,
     (error) => {
       if (error?.response?.status === UNAUTHORIZED) {
-        // dispatchAuth({ type: "LOGOUT_USER" });
         console.log("error handler ran successsfully");
         localStorage.clear();
         navigate("/access", { replace: true });
-        // return true;
       }
       return Promise.reject(error);
     }
