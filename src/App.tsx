38443e0372c3def06bc678884bfb7e2ca30ed53c
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-23 11:53:15 +0530

    update: initiated axios interceptor for unauthorized access

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
 import { Access } from "./pages/Access/Access";
 import { Categories } from "./pages/Categories/Categories";
 import { Header } from "./Components/Header/Header";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { SignUp } from "./pages/SignUp/SignUp";
 import { Question } from "./pages/Question/Question";
 import { Results } from "./pages/Results/Results";
 import { Leaderboard } from "./pages/Leaderboard/Leaderboard";
 import { Homepage } from "./pages/Homepage/Homepage";
 import { useEffect } from "react";
-import { QuizData } from "./ApiCalls/userAuth";
+import { QuizData, setupUserAuthorizationHandler } from "./ApiCalls/userAuth";
 import { useData } from "./context/quizdata-context";
 function App() {
+  const navigate = useNavigate();
   const { data, setData } = useData();
   function PrivateRoute(props: any) {
     if (localStorage.getItem("token")) {
@@ -25,6 +26,7 @@ function App() {
     async function Run() {
       const response = await QuizData();
       setData({ ...data, quizCategories: response });
+      setupUserAuthorizationHandler(navigate);
     }
     Run();
   }, []);
