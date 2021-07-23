fb38285451f0d8839f787492a4076daa63bd460d
Syed Andleeb <andleebsyed75@gmail.com>
2021-07-23 16:32:56 +0530

    update: customized homepage so that user gets to optimal page depending upon if user is logged in or not

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 export function Homepage() {
+  const homePageRedirector: string = localStorage.getItem("token")
+    ? "/categories"
+    : "/access";
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-grey">
       <h1 className="text-red font-bold text-4xl">Enigma!</h1>
@@ -8,7 +11,7 @@ export function Homepage() {
         An interactive Quiz where you can test your knowledge about different
         fields
       </p>
-      <Link to="/access">
+      <Link to={`${homePageRedirector}`}>
         <button className="bg-red text-white font-bold p-4 pl-14 pr-14 rounded mt-8">
           Let's Play
         </button>
